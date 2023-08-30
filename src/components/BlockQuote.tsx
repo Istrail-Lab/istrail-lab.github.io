d46239c906c8516704033715d05252ab6813e181
Hammad Izhar <hammad_izhar@brown.edu>
2023-08-30 02:31:28 -0400

    research accordion finished

--- src/components/BlockQuote.tsx
+++ src/components/BlockQuote.tsx
@@ -1,11 +1,12 @@
 const BlockQuote = ({
     author,
+    journal,
     children,
-}: React.PropsWithChildren<{ author: string }>) => {
+}: React.PropsWithChildren<{ author: string; journal: string }>) => {
     return (
         <blockquote className="p-4 my-4 border-l-4 border-gray-300 bg-gray-50 flex">
             <svg
-                className="w-8 h-8 text-gray-400/50 rotate-180 m-4 flex-shrink-0"
+                className="hidden md:visible w-8 h-8 text-gray-400/50 rotate-180 m-4 flex-shrink-0"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -13,23 +14,15 @@ const BlockQuote = ({
             >
                 <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
             </svg>
-            <div className="mt-8 flex-grow">
+            <div className="md:mt-8 flex-grow">
                 <i>{children}</i>
                 <br />
                 <br />
-                <span className="flex justify-end font-bold text-md">
-                    {author}
-                </span>
+                <div className="flex flex-col items-end text-md">
+                    <p className="font-bold">{author}</p>
+                    <p className="italic">{journal}</p>
+                </div>
             </div>
-            {/* <svg
-                className="w-8 h-8 text-gray-400/50 mb-4 absolute -z-10 bottom-0 right-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 18 14"
-            >
-                <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-            </svg> */}
         </blockquote>
     );
 };
--- src/components/ResearchAccordion.tsx
+++ src/components/ResearchAccordion.tsx
@@ -14,7 +14,7 @@ const researchProblems = [
                 Phasing, Imputation, Assembly, GWAS, Diploid, Haploid,
                 Metagenomics, Epigenomics, HIV patient metagenome.
             </p>
-            <BlockQuote author="J. C. Venter">
+            <BlockQuote author="J. C. Venter" journal="Nature, 2010">
                 Improving data quality is crucial, because if a human genome
                 cannot be independently assembled then the sequence data cannot
                 be sorted into the two sets of parental chromosomes, or
@@ -25,22 +25,76 @@ const researchProblems = [
                 heritability, gene function, regulatory sequences and our
                 predisposition to disease.
             </BlockQuote>
-            <BlockQuote author="Nature">
+            <BlockQuote author="J. C. Venter" journal="Nature, 2010">
                 Multiple personal genomes await.
             </BlockQuote>
         </>,
     ],
     [
         "The Regulatory Genome and the Computer",
-        'The definitive feature of the many thousand cis-regulatory control modules in an animal genome is their information processing capability. In the "genomic computer" intra-machine communication occurs by means of diffusion (of transcription factors), while in electronic computers it occurs by electron transit along pre-organized wires. There follow fundamental differences in design principle in respect to the meaning of time, speed, multiplicity of processors, memory, robustness of computation and hardware and software. The genomic computer controls spatial gene expression in the development of the body plan, and its appearance in remote evolutionary time must be considered to have been a founding requirement for animal grade life.',
+        <>
+            <p>
+                The definitive feature of the many thousand cis-regulatory
+                control modules in an animal genome is their information
+                processing capability. In the "genomic computer" intra-machine
+                communication occurs by means of diffusion (of transcription
+                factors), while in electronic computers it occurs by electron
+                transit along pre-organized wires. There follow fundamental
+                differences in design principle in respect to the meaning of
+                time, speed, multiplicity of processors, memory, robustness of
+                computation and hardware and software. The genomic computer
+                controls spatial gene expression in the development of the body
+                plan, and its appearance in remote evolutionary time must be
+                considered to have been a founding requirement for animal grade
+                life.
+            </p>
+        </>,
     ],
     [
         "Genetic Heterogeneity and the Missing Heritability Problem",
-        'The genetic architecture of a disorder, i.e., the number and frequency of susceptibility alleles, is shaped by evolution. The majority of variants detected by Genome-Wide Association Studies (GWAS) have no demonstrated biological significance. "The general failure to confirm common risk variants is not due to a failure to carry out GWAS properly. The problem is underlying biology, not the operationalization of study design. The common disease—common variant model has been the primary focus of human genomics over the last decade. .. If common alleles influenced common diseases, many would have been found by now. The issue is not how to develop still larger studies, or how to parse the data still further, but rather whether the common disease—common variant hypothesis has now been tested and found not to apply to most complex human diseases." Jon McClellan, and Mary-Claire King, "Genetic Heterogeneity in Human Disease" Cell, 2010',
+        <>
+            <p>
+                The genetic architecture of a disorder, i.e., the number and
+                frequency of susceptibility alleles, is shaped by evolution. The
+                majority of variants detected by Genome-Wide Association Studies
+                (GWAS) have no demonstrated biological significance.
+            </p>
+            <BlockQuote
+                author="Jon McClellan and Mary-Claire King"
+                journal="Cell, 2010"
+            >
+                The general failure to confirm common risk variants is not due
+                to a failure to carry out GWAS properly. The problem is
+                underlying biology, not the operationalization of study design.
+                The common disease—common variant model has been the primary
+                focus of human genomics over the last decade,..., if common
+                alleles influenced common diseases, many would have been found
+                by now. The issue is not how to develop still larger studies, or
+                how to parse the data still further, but rather whether the
+                common disease—common variant hypothesis has now been tested and
+                found not to apply to most complex human diseases.
+            </BlockQuote>
+        </>,
     ],
     [
         "Protein Folding Algorithms",
-        "The search for combinatorial algorithms for lattice protein folding that construct the lowest energy fold with mathematical guaranteed error bounds are illuminating the elusive structure of optimal folds. Although for almost every model, protein folding was proved NP-complete, the search for the optimal algorithm that would find the optimum fold on lattice proteins with real protein sequences from PDB is under way. A major unresolved problem is the protein energy function inference. We used methods from Economics and Political Science, namely, Voting Theory, to infer from individual preferences of amino acids in PDB protein structures, the social choice postulated by the thermodynamic hypothesis, namely the existence of a universal energy function ",
+        <>
+            <p>
+                The search for combinatorial algorithms for lattice protein
+                folding that construct the lowest energy fold with mathematical
+                guaranteed error bounds are illuminating the elusive structure
+                of optimal folds. Although for almost every model, protein
+                folding was proved NP-complete, the search for the optimal
+                algorithm that would find the optimum fold on lattice proteins
+                with real protein sequences from PDB is under way. A major
+                unresolved problem is the protein energy function inference. We
+                used methods from Economics and Political Science, namely,
+                Voting Theory, to infer from individual preferences of amino
+                acids in PDB protein structures, the social choice postulated by
+                the thermodynamic hypothesis, namely the existence of a
+                universal energy function.
+            </p>
+        </>,
     ],
 ] as const;
 
@@ -55,7 +109,9 @@ const ResearchAccordion = () => {
             {researchProblems.map(([title, content]) => (
                 <AccordionItem
                     className="px-4 border-b-2 border-b-gray-400/50"
+                    style={{ textWrap: "balance" } as React.CSSProperties}
                     value={title}
+                    key={title}
                 >
                     <AccordionTrigger className="flex gap-10 text-left font-bold">
                         {title}
