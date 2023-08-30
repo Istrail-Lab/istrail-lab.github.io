0d897e6db4776ae2b86f7ad7d9eae5bc02261262
Hammad Izhar <hammad_izhar@brown.edu>
2023-08-30 14:52:57 -0400

    honor roll students

---
+++ src/components/honorRoll/HonorRollList.tsx
@@ -0,0 +1,27 @@
+import type { HonorRollStudent } from "./honorRoll";
+
+const HonorRollCard = ({ student }: { student: HonorRollStudent }) => {
+    return (
+        <div className="border-2 shadow-lg p-4 flex flex-col justify-center text-center">
+            <h3 className="font-bold text-xl p-2">{student.name}</h3>
+            <ul>
+                {student.achievements.map((achievement) => (
+                    <li>{achievement}</li>
+                ))}
+            </ul>
+            <p className="italic p-2">{student.currentLocation}</p>
+        </div>
+    );
+};
+
+const HonorRollList = ({ students }: { students: HonorRollStudent[] }) => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {students.map((student) => (
+                <HonorRollCard student={student} />
+            ))}
+        </div>
+    );
+};
+
+export default HonorRollList;
---
+++ src/components/honorRoll/honorRoll.ts
@@ -0,0 +1,163 @@
+export interface HonorRollStudent {
+    name: string;
+    achievements: string[];
+    currentLocation: string;
+}
+
+export const students: HonorRollStudent[] = [
+    {
+        name: "Sud Perera",
+        achievements: ["Honor Thesis 2017"],
+        currentLocation: "Medical Student at Brown Medical School",
+    },
+    {
+        name: "Daniel Seidman",
+        achievements: ["Honor Thesis 2016"],
+        currentLocation: "Graduate Student at Cornell University",
+    },
+    {
+        name: "Samuel Angelo Crisanto",
+        achievements: ["Honor Thesis 2016"],
+        currentLocation: "Researcher at Microsoft Research, Cambridge, MA",
+    },
+    {
+        name: "Younhun Kim",
+        achievements: ["Honor Thesis 2016", "Presidential Fellowship"],
+        currentLocation: "Graduate Student at MIT",
+    },
+    {
+        name: "Andy Ly",
+        achievements: ["Honor Thesis 2016"],
+        currentLocation: "Masters Student at Brown",
+    },
+    {
+        name: "Jens Sannerud",
+        achievements: ["Honors Thesis 2016"],
+        currentLocation: "Graduate Student at Cornell University",
+    },
+    {
+        name: "Derek Aguiar",
+        achievements: ["Second PhD student at Brown, graduated May 2014"],
+        currentLocation: "Postdoctoral Fellow at Princeton University",
+    },
+    {
+        name: "Ryan Tarpine",
+        achievements: ["First PhD student at Brown, graduated May 2012"],
+        currentLocation: "researcher at Google",
+    },
+    {
+        name: "Austin Huang",
+        achievements: [
+            "Postdoctoral Fellow",
+            "co-advised with Professor Rami Kantor MD, Brown Medical School and Professor Joe Hogan, School of Public Health, 2014",
+        ],
+        currentLocation: "Senior Scientist at Pfizer Research",
+    },
+    {
+        name: "Alper Uzun",
+        achievements: [
+            "Postodoctoral Fellow",
+            "co-advised with professor Jim Padbury, MD, Brown Medical School, 2014",
+        ],
+        currentLocation: "Assistant Professor at Brown Medical School",
+    },
+    {
+        name: "Fumei Lam",
+        achievements: ["Postdoctoral Fellow 2012"],
+        currentLocation: "IDA, Princeton",
+    },
+    {
+        name: "Ning Hou",
+        achievements: [
+            "Randy Pausch Computer Science UTRA 2013 recipient",
+            "Honor Thesis 2014",
+            "Masters student in Computer Science at Brown",
+        ],
+        currentLocation: "Amazon",
+    },
+    {
+        name: "Nick Goelz",
+        achievements: ["UTRA award recipient", "Honor Thesis 2014"],
+        currentLocation: "",
+    },
+    {
+        name: "Doug McErlean",
+        achievements: [
+            "top Honor Thesis in the CS Department 2013",
+            "Magna Cum Laude",
+            "CRA Honorable Mention",
+        ],
+        currentLocation: "Software Engineer at Google",
+    },
+    {
+        name: "Kshitij Lauria",
+        achievements: ["Honor Thesis 2014"],
+        currentLocation: "D. E. Shaw Research",
+    },
+    {
+        name: "Jake Franco",
+        achievements: ["Honor Thesis"],
+        currentLocation: "Medical Student at Stony Brook Medical School",
+    },
+    {
+        name: "Tim Johnstone",
+        achievements: [
+            "Leader of Cyrene Project biology team",
+            "Honor Thesis",
+            "Biology Prize",
+        ],
+        currentLocation: "Graduate Student at Yale University",
+    },
+    {
+        name: "James Weis",
+        achievements: ["Honor Thesis"],
+        currentLocation: "Graduate Student at MIT",
+    },
+    {
+        name: "James Hart",
+        achievements: ["Leader of the Cyrene Project biology team"],
+        currentLocation: "Graduate Student at U.C. Berkeley",
+    },
+    {
+        name: "Alex Gillmore",
+        achievements: ["Masters Student"],
+        currentLocation: "Yelp",
+    },
+    {
+        name: "Kyle Schutter",
+        achievements: [
+            "Leader of the Cyrene Project biology team",
+            "Honor Thesis",
+            "Thesis Award",
+        ],
+        currentLocation: "Business Entrepreneur in Kenya",
+    },
+    {
+        name: "William Allen",
+        achievements: ["TA and Cyrene Project member", "Marshall Fellowship"],
+        currentLocation: "Graduate Student at Stanford University",
+    },
+    {
+        name: "Christopher Maloney",
+        achievements: [],
+        currentLocation: "Microsoft",
+    },
+    {
+        name: "Allan Stewart",
+        achievements: ["George I. Alden Trust UTRA Award", "Honor Thesis"],
+        currentLocation: "Facebook",
+    },
+    {
+        name: "David Moskowitz",
+        achievements: ["endowed and named UTRA Award", "Cyrene Project member"],
+        currentLocation: "Graduate Student at Stanford University",
+    },
+    {
+        name: "Lian Garton",
+        achievements: [
+            "First student at Brown",
+            "SC Lamport Honor Thesis Award",
+        ],
+        currentLocation: "High-Level Manager at Amazon",
+    },
+];
