66ad48425308d59c241956a0d9ad82c3afced612
Hammad Izhar <hammad_izhar@brown.edu>
2023-09-26 16:25:04 -0400

    use ruffle to embed swf, but having some trouble so get back to this

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -16,7 +16,7 @@ const QuickLinks = () => {
             <Link href="/sorin-istrail/teaching">Teaching</Link>
             <Link href="/sorin-istrail/honors">Honors & Awards</Link>
             <Link href="/sorin-istrail/grants">Grants</Link>
-            <Link href="/sorin-istrail/geneaology">Advisor Geneaology</Link>
+            <Link href="/sorin-istrail/geneology">Advisor Geneology</Link>
         </aside>
     );
 };
