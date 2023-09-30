72421f27acdb261f2404dda33633555e40f7af42
Hammad Izhar <hammad_izhar@brown.edu>
2023-09-30 12:25:22 -0400

    update carousel pictures

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,7 +2,7 @@ import MUICarousel from "react-material-ui-carousel";
 
 const Carousel = ({ images }: { images: string[] }) => {
     return (
-        <MUICarousel interval={3000} navButtonsAlwaysVisible indicators={false}>
+        <MUICarousel interval={3000} indicators={false}>
             {images.map((image, index) =>
                 image.split(".")[1] === "mov" ? (
                     <video key={image} src={image} className="mx-auto h-80" />
