c74e371be817325e466d22c8de77245cccccf28e
Hammad Izhar <hammad_izhar@brown.edu>
2023-09-30 14:42:45 -0400

    update honor roll to include all new students

--- src/components/honorRoll/honorRoll.ts
+++ src/components/honorRoll/honorRoll.ts
@@ -1,3 +1,5 @@
+import { SecurityUpdateWarning } from "@mui/icons-material";
+
 export interface HonorRollStudent {
     name: string;
     achievements: string[];
@@ -6,158 +8,226 @@ export interface HonorRollStudent {
 
 export const students: HonorRollStudent[] = [
     {
-        name: "Sud Perera",
-        achievements: ["Honor Thesis 2017"],
-        currentLocation: "Medical Student at Brown Medical School",
-    },
-    {
-        name: "Daniel Seidman",
-        achievements: ["Honor Thesis 2016"],
-        currentLocation: "Graduate Student at Cornell University",
-    },
-    {
-        name: "Samuel Angelo Crisanto",
-        achievements: ["Honor Thesis 2016"],
-        currentLocation: "Researcher at Microsoft Research, Cambridge, MA",
-    },
-    {
-        name: "Younhun Kim",
-        achievements: ["Honor Thesis 2016", "Presidential Fellowship"],
-        currentLocation: "Graduate Student at MIT",
-    },
-    {
-        name: "Andy Ly",
-        achievements: ["Honor Thesis 2016"],
-        currentLocation: "Masters Student at Brown",
-    },
-    {
-        name: "Jens Sannerud",
-        achievements: ["Honors Thesis 2016"],
-        currentLocation: "Graduate Student at Cornell University",
-    },
-    {
-        name: "Derek Aguiar",
-        achievements: ["Second PhD student at Brown, graduated May 2014"],
-        currentLocation: "Assistant Professor at UConn",
-    },
-    {
-        name: "Ryan Tarpine",
-        achievements: ["First PhD student at Brown, graduated May 2012"],
-        currentLocation: "researcher at Google",
-    },
-    {
-        name: "Austin Huang",
-        achievements: [
-            "Postdoctoral Fellow",
-            "co-advised with Professor Rami Kantor MD, Brown Medical School and Professor Joe Hogan, School of Public Health, 2014",
-        ],
-        currentLocation: "Google DeepMind",
-    },
-    {
-        name: "Alper Uzun",
-        achievements: [
-            "Postodoctoral Fellow",
-            "co-advised with professor Jim Padbury, MD, Brown Medical School, 2014",
-        ],
-        currentLocation: "Assistant Professor at Brown Medical School",
-    },
-    {
-        name: "Fumei Lam",
-        achievements: ["Postdoctoral Fellow 2012"],
-        currentLocation: "IDA, Princeton",
-    },
-    {
-        name: "Ning Hou",
-        achievements: [
-            "Randy Pausch Computer Science UTRA 2013 recipient",
-            "Honor Thesis 2014",
-            "Masters student in Computer Science at Brown",
-        ],
-        currentLocation: "Amazon",
-    },
-    {
-        name: "Nick Goelz",
-        achievements: ["UTRA award recipient", "Honor Thesis 2014"],
+        name: "Pinar Demetci",
+        achievements: ["PhD student co-advised with Ritambhara Singh"],
         currentLocation: "",
     },
     {
-        name: "Doug McErlean",
+        name: "Derek Aguiar",
         achievements: [
-            "top Honor Thesis in the CS Department 2013",
-            "Magna Cum Laude",
-            "CRA Honorable Mention",
+            '"The ARIADNE Browser for Genome-Wide Association Studies" Master\'s Thesis',
+            '"Genome-wide Algorithms for Haplotype Assembly, Haplotype Phasing, and Identical-by-Descent Inference" PhD thesis defended in May 2014',
+            "Postdoctoral Student at Princeton University",
+        ],
+        currentLocation: "Assistant Professor at University of Connecticut",
+    },
+    {
+        name: "Ryam Tarpine",
+        achievements: [
+            '"The CYRENE cisGRN Browser for the Regulatory Genome" Master\'s Thesis',
+            '"A Database of Causality-Inferred Structure-Function Information for Genomic cis-Regulatory Architecture," PhD thesis defended in May 2012',
         ],
         currentLocation: "Software Engineer at Google",
     },
     {
-        name: "Kshitij Lauria",
-        achievements: ["Honor Thesis 2014"],
-        currentLocation: "D. E. Shaw Research",
-    },
-    {
-        name: "Jake Franco",
-        achievements: ["Honor Thesis"],
-        currentLocation: "Medical Student at Stony Brook Medical School",
-    },
-    {
-        name: "Tim Johnstone",
+        name: "Dejan Zivkovic",
         achievements: [
-            "Leader of Cyrene Project biology team",
-            "Honor Thesis",
-            "Biology Prize",
+            '"Bounded-Width Polynomial Size Boolean Formuals Compute Exactly Those Functions from AC^0" PhD thesis defended in May 1994',
         ],
-        currentLocation: "Graduate Student at Yale University",
+        currentLocation: "Professor of Computer Science in Serbia",
     },
     {
-        name: "James Weis",
-        achievements: ["Honor Thesis"],
-        currentLocation: "Graduate Student at MIT",
+        name: "Fumei Lam",
+        achievements: ["Postdoctoral Student"],
+        currentLocation: "UC Davis/UC Berkeley",
     },
     {
-        name: "James Hart",
-        achievements: ["Leader of the Cyrene Project biology team"],
-        currentLocation: "Graduate Student at U.C. Berkeley",
-    },
-    {
-        name: "Alex Gillmore",
-        achievements: ["Masters Student"],
-        currentLocation: "Yelp",
-    },
-    {
-        name: "Kyle Schutter",
+        name: "Austin Huang",
         achievements: [
-            "Leader of the Cyrene Project biology team",
-            "Honor Thesis",
-            "Thesis Award",
+            "Postdoctoral Student",
+            "Researcher at Pfizer Research Center",
+            "Vice President of Artifical Intelligence and Machine Learning at Fidelity Investments",
         ],
-        currentLocation: "Business Entrepreneur in Kenya",
+        currentLocation: "Google DeepMind",
     },
     {
-        name: "William Allen",
-        achievements: ["TA and Cyrene Project member", "Marshall Fellowship"],
-        currentLocation: "Graduate Student at Stanford University",
+        name: "Ning Hou",
+        achievements: [
+            '"Two Problems Related to cis-Regulatory Architecture of Transcription Factor Encoding Genes Homologous Translation and Evolutionary Conservation-Based cis-Module Inference" Master\'s Thesis (2014)',
+        ],
+        currentLocation: "Amazon",
     },
     {
-        name: "Christopher Maloney",
-        achievements: [],
-        currentLocation: "Microsoft",
-    },
-    {
-        name: "Allan Stewart",
-        achievements: ["George I. Alden Trust UTRA Award", "Honor Thesis"],
-        currentLocation: "Facebook",
-    },
-    {
-        name: "David Moskowitz",
-        achievements: ["endowed and named UTRA Award", "Cyrene Project member"],
-        currentLocation: "Graduate Student at Stanford University",
+        name: "Lan Nguyen",
+        achievements: [
+            '"Survey on Protein Folding Algorithms of HP Model and Proposal of a Hybrid Heuristic Algorithm" Master\'s Thesis (2019)',
+        ],
+        currentLocation: "Startup Software Company in Massachusetts",
     },
     {
         name: "Lian Garton",
         achievements: [
-            "First student at Brown",
-            "SC Lamport Honor Thesis Award",
+            '"Population Substructure and MCMC" (S.C. Lamport Honor Thesis Award, 2008)',
         ],
-        currentLocation: "High-Level Manager at Amazon",
+        currentLocation: "Amazon",
+    },
+    {
+        name: "Kyle Schutter",
+        achievements: [
+            '"The cis-Regulatory Genomics Quintessential Graph Problem" (2010)',
+        ],
+        currentLocation: "Founded Startup in Africa",
+    },
+    {
+        name: "Allan Stewart",
+        achievements: [
+            '"Face-Centered Cubic Lattice Models for Protein Folding: Energy Function Inference and Biplane Packing (2010)"',
+        ],
+        currentLocation: "Facebook",
+    },
+    {
+        name: "Tim Johnstone",
+        achievements: [
+            '"Gene Networks: An Algorithm to Discover Causal Relationships Through Systematic Experimentation" (Senior Prize for Biology, 2012)',
+        ],
+        currentLocation: "PhD Student at Yale University",
+    },
+    {
+        name: "Jake Franco",
+        achievements: ['"MCMC Algorithms for Haplotype Phasing" (2012)'],
+        currentLocation: "Stony Brook Medical School",
+    },
+    {
+        name: "James Weiss",
+        achievements: [
+            '"Computational Genomics and Bioenergy: Modeling and Clustering of RNA-seq Data" (2012)',
+        ],
+        currentLocation: "PhD Student at MIT",
+    },
+    {
+        name: "Jeffrey Herman",
+        achievements: [
+            '"A Markov Random Field Model for Inferring Population Structure" (2012)',
+        ],
+        currentLocation: "Google",
+    },
+    {
+        name: "Andy Ly",
+        achievements: [
+            '"Towards Unifying Tagging SNP Selection Algorithms" (2016)',
+        ],
+        currentLocation: "Software Engineer at Google Brain",
+    },
+    {
+        name: "Sudheesha Perera",
+        achievements: [
+            '"A Haplotype-Based Predictive Model for Genotype/Expression Datasets" (2017)',
+        ],
+        currentLocation: "Brown Medical School",
+    },
+    {
+        name: "Nick Goelz",
+        achievements: [
+            '"Exact Backtracking Algorithms for Optimal Bipole Packing in HP Model of Protein Folding" (2015)',
+        ],
+        currentLocation: "Full Stack Engineer",
+    },
+    {
+        name: "Alex Gillmore",
+        achievements: ['"Linear Models for SNPs selection" (2012)'],
+        currentLocation: "Yelp",
+    },
+    {
+        name: "Douglas McErlean",
+        achievements: [
+            '"One Constraint to Rule Them All: How to Simplify Optimizations Under Constant Variable Sum, with Applications for Maximum Likelihood" (Top Honors Thesis, CS Department, 2014)',
+        ],
+        currentLocation: "Google",
+    },
+    {
+        name: "Kshitij Lauria",
+        achievements: [
+            '"Bipole Self-Assembly and the Biplane Conjecture" (2015)',
+        ],
+        currentLocation: "D. E. Shaw Research",
+    },
+    {
+        name: "Ning Hou",
+        achievements: [
+            '"Two Problems Releated to cis-Regulatory Architecture of Transcription Factor Encoding Genes: Homologous Translation and Evolutionary Conservation-Based cis-Module Inference" (2017)',
+        ],
+        currentLocation: "Amazon",
+    },
+    {
+        name: "Daniel Seidman",
+        achievements: [
+            '"LumberTracts: A Method for TIme Efficient Determination of Identical by Descent Tracts Between Unphased Genotypes" (2016)',
+        ],
+        currentLocation: "PhD Student at Cornell University",
+    },
+    {
+        name: "Sam Crisanto",
+        achievements: [
+            '"HapROSEA Software Package for Long Range Haplotype Phasing" (2016)',
+        ],
+        currentLocation: "Microsoft Research, Cambridge, MA",
+    },
+    {
+        name: "Youn Kim",
+        achievements: [
+            '"Algebraic Connectivity of Graphs with Applications to Protein Folding" (2016)',
+        ],
+        currentLocation: "PhD Student at MIT with Presidential Fellowship",
+    },
+    {
+        name: "Marko Fejzo",
+        achievements: ['"Spectral Graph Theory and Markov Chains" (2019)'],
+        currentLocation: "TwoSigma",
+    },
+    {
+        name: "Shivam Nadimpalli",
+        achievements: ['"Discrete Isoperimetry and Protein Folding" (2019)'],
+        currentLocation: "PhD Student at Columbia University",
+    },
+    {
+        name: "Sam Hinthorn",
+        achievements: [
+            '"cis-Lexicon Ontology Search Engine for Gene Regulatory Networks" (2019)',
+        ],
+        currentLocation: "Biology PhD Student at Brown University",
+    },
+    {
+        name: "Bethany Dubois",
+        achievements: [
+            "Metropolois-Coupled Monte Carlo in Application to Protein Structure Prediction (2018)",
+            "D. E. Shaw Research",
+        ],
+        currentLocation: "Mount Sinai Medical School",
+    },
+    {
+        name: "Adrian Turcu",
+        achievements: [
+            '"Protein Folding Prediction and Visualization Techniques Based on Hydrophobic Side Chain Interactions" (2019)',
+        ],
+        currentLocation: "Brown Medical School",
+    },
+    {
+        name: "Min Jean Cho",
+        achievements: [
+            '"Genomic Privacy Algorithms Based on Information Theory" (2020)',
+        ],
+        currentLocation: "Intel",
+    },
+    {
+        name: "Arun Das",
+        achievements: ['"Approaches to Genomic Privacy (2018)"'],
+        currentLocation: "Johns Hopkins Medical School",
+    },
+    {
+        name: "Daniel Ben-Isvy",
+        achievements: [
+            '"Multilocus Informativeness: A Novel Method for Fine-Mapping Disease-Causing Variants in Genome-Wide Association Studies" (2020)',
+        ],
+        currentLocation: "Harvard Medical School",
     },
 ];
