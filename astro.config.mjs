53dbc4daa1ff9e25a4e809306b7010e186c482bf
cdbakes <colindbaker7@gmail.com>
2025-02-17 19:00:10 -0500

    deploy update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-// https://astro.build/config
+// Use environment variables to set the site URL and base path.
+const site = process.env.ASTRO_SITE || "https://istrail-laboratory.github.io";
+const base = process.env.ASTRO_BASE || "/";
+
 export default defineConfig({
-    site: "https://istrail-laboratory.github.io",
-    integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        react(),
-    ],
-    build: {
-        rollupOptions: {
-            onwarn(warning, warn) {
-                if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-                    return;
-                }
-                warn(warning);
-            },
-        },
+  site,
+  base,
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") return;
+        warn(warning);
+      },
     },
-    assetsInclude: ["**/*.mov"],
+  },
+  assetsInclude: ["**/*.mov"],
 });
